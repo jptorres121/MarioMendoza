0520f193e211abe8c6afb3519e06082db0929e9b
jptorres121 <jptorres454@gmailcom>
2025-06-04 16:19:53 -0500

    Base de datos

--- app/database.py
+++ app/database.py
@@ -1,13 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./obras.db"
+# Cargar la URL desde las variables de entorno, si no existe usar SQLite localmente
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./obras.db")
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+# Para PostgreSQL no se necesita connect_args
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Dependency para obtener la sesión de base de datos
 def get_db():
     db = SessionLocal()
     try:
